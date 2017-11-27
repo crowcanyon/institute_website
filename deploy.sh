d67911130a7c075f3c0ac959392be46b00e52ed6
bocinsky <bocinsky@gmail.com>
2017-11-27 11:01:15 -0700

    Added publications list

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ## Deploy
 
-mkdir /Volumes/institute/
-mount_smbfs smb://$1:$2@www/institute/ /Volumes/institute/
-rsync -rv --size-only --delete ./_site/ /Volumes/institute/
-umount /Volumes/institute/
+mkdir ~/institute/
+mount_smbfs smb://$1:$2@www/institute/ ~/institute/
+rsync -rv --size-only --delete ./_site/ ~/institute/
+umount ~/institute/
+rm -r ~/institute/
