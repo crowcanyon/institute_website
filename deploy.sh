e18000df1df7bc9094f780f855a0db7ba51834cb
bocinsky <bocinsky@gmail.com>
2018-03-27 11:13:09 -0600

    Added Stefani Crabtree, and removed Watkins, Lorusso, and Gangloff

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 ## Deploy
 
 mkdir ~/institute/
-mount_smbfs smb://$1:$2@www/institute/ ~/institute/
+mount_smbfs //$1:$2@www/institute/ ~/institute/
 rsync -rv --size-only --delete ./_site/ ~/institute/
 umount ~/institute/
 rm -r ~/institute/
