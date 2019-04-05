e1cb502788971b9d10ca95b66c98d3f0b417a43f
R. Kyle Bocinsky <bocinsky@gmail.com>
2019-04-05 11:50:04 -0600

    staffing changes

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ## Deploy
 
+bash rebuild.sh
+
 find . -name '.DS_Store' -type f -delete
 find . -name 'Icon*' -type f -delete
 
 mkdir ~/institute/
-mount_smbfs //$1:$2@www/LiveWebsite/institute/ ~/institute/
+mount_smbfs //$ccac_un:$ccac_pw@www/LiveWebsite/institute/ ~/institute/
 rsync -rv --size-only --delete ./_site/ ~/institute/
 umount ~/institute/
 rm -r ~/institute/
